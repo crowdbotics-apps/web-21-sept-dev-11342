eb612a6a65686a9105e511e42e8386250969addd
Crowdbotics <team@crowdbotics.com>
2020-09-21 06:47:47 +0000

    Update models

--- home/models.py
+++ home/models.py
@@ -10,6 +10,13 @@ class CustomText(models.Model):
     title = models.CharField(
         max_length=150,
     )
+    hgjfhgg = models.OneToOneField(
+        "home.CustomText",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="customtext_hgjfhgg",
+    )
 
     def __str__(self):
         return self.title
@@ -38,3 +45,15 @@ class HomePage(models.Model):
 class Hmhgfhgf(models.Model):
     "Generated Model"
     jhgjhgj = models.BigIntegerField()
+    hgfhgfhgfh = models.ForeignKey(
+        "home.HomePage",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="hmhgfhgf_hgfhgfhgfh",
+    )
+    hgfhfgfhgf = models.ManyToManyField(
+        "users.User",
+        blank=True,
+        related_name="hmhgfhgf_hgfhfgfhgf",
+    )
